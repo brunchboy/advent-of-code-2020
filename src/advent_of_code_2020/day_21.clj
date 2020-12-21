215a4fdc09ce82c28f34d3b566bd4af7af69c983
James Elliott <james@deepsymmetry.org>
2020-12-21 00:32:04 -0600

    Add day 21 part 1 (still lacking doc strings).

---
+++ src/advent_of_code_2020/day_21.clj
@@ -0,0 +1,52 @@
+(ns advent-of-code-2020.day-21
+  "Solutions for day 21."
+  (:require [clojure.java.io :as io]
+            [clojure.set :as set]
+            [clojure.string :as str]))
+
+(def input "The food list (puzzle input)."
+  (-> (io/reader (io/resource "day_21.txt"))
+      line-seq))
+
+(defn parse-line
+  [line]
+  (let [[_ ingredients allergens] (re-matches #"(.*)\s+\(contains\s+(.*)\)" line)]
+    [(set (str/split ingredients #"\s+")) (set (str/split allergens #",\s+"))]))
+
+(defn init-potentials
+  [all-ingredients all-allergens]
+  (reduce (fn [acc allergen]
+            (assoc acc allergen all-ingredients))
+          {}
+          all-allergens))
+
+(defn remove-ingredient-from-allergens
+  [potentials ingredient allergens]
+  (reduce (fn [acc allergen]
+            (update acc allergen disj ingredient))
+          potentials
+          allergens))
+
+(defn winnow
+  [all-ingredients potentials [ingredient-set allergen-set]]
+  (reduce (fn [acc ingredient]
+             (if (ingredient-set ingredient)
+               acc
+               (remove-ingredient-from-allergens acc ingredient allergen-set)))
+           potentials
+           all-ingredients))
+
+(defn part-1
+  ([]
+   (part-1 input))
+  ([foods]
+   (let [parsed          (map parse-line foods)
+         ingredient-sets (map first parsed)
+         allergen-sets   (map second parsed)
+         all-ingredients (apply set/union ingredient-sets)
+         all-allergens   (apply set/union allergen-sets)
+         potentials      (reduce (partial winnow all-ingredients)
+                                 (init-potentials all-ingredients all-allergens)
+                                 parsed)
+         safe            (set/difference all-ingredients (apply set/union (vals potentials)))]
+     (apply + (map count (map #(set/intersection safe %) ingredient-sets))))))
---
+++ test/advent_of_code_2020/day_21_test.clj
@@ -0,0 +1,16 @@
+(ns advent-of-code-2020.day-21-test
+  "Unit tests for day 21."
+  (:require [clojure.string :as str]
+            [clojure.test :as test]
+            [advent-of-code-2020.day-21 :as sut]))
+
+(def sample-input
+  "The food list for the sample problem."
+  (-> "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)"
+      (str/split-lines)))
+
+(test/deftest part-1
+  (test/is (= (sut/part-1 sample-input) 5)))
