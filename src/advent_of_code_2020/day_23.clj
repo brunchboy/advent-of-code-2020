9e69f032b9ac3a8d60db3e54d032133ff135f37e
James Elliott <james@deepsymmetry.org>
2020-12-23 17:23:45 -0600

    Separate inefficient day 23 solution into its own file.
    Also test both implementations on the sample problem. This makes it
    easier to understand and talk about both implementations.

--- src/advent_of_code_2020/day_23.clj
+++ src/advent_of_code_2020/day_23.clj
@@ -6,53 +6,6 @@
   "The cup ordering (puzzle input)."
   [5 9 8 1 6 2 7 3 4])
 
-(defn find-destination
-  "Given the label of the current cup and the cups remaining after three
-  have been picked up, find the label of the destination cup where
-  they should be placed, given the rules in the problem statement.
-  Choose the first available cup whose label is less than the current
-  cup, wrapping to the highest label if we hit zero."
-  [current others]
-  (let [others (set others)]
-    (loop [result (if (> current 1) (dec current) 9)]
-      (if (others result)
-        result
-        (recur (if (> result 1) (dec result) 9))))))
-
-(defn move
-  "Perform a single move in the game of crab cups. Remove three cups
-  after the current (first) cup, find the destination cup after which
-  they should be placed, and move them there. Leave the next current
-  cup as the first cup in the list returned so we can always just work
-  from the front (since they are arranged in a circle, this is
-  valid)."
-  [cups]
-  (let [[current & others] cups
-        picked (take 3 others)
-        others (drop 3 others)
-        [before after] (split-at (inc (.indexOf others (find-destination current others))) others)]
-    (concat before picked after [current])))
-
-(defn part-1
-  "Solve part 1: Play 100 moves, then return the labels of the cups that
-  follow cup 1 as a string."
-  ([]
-   (part-1 input))
-  ([cups]
-   (let [result (nth (iterate move cups) 100)
-         [before after] (split-at (.indexOf result 1) result)]
-     (apply str (concat (drop 1 after) before)))))
-
-;; Because part 2 involves a vastly larger number of cups and moves,
-;; we need much more efficient data structures too. These functions
-;; below can solve part 1 as well, and I considered just replacing the
-;; above versions with these new ones, but I decided to keep both
-;; around for comparison.
-
-(def part-2-input
-  "The huge input for part 2."
-  (concat input (range 10 1000001)))
-
 (defn build-links
   "We represent our cups as a circular linked hash map. Each key in the
   map is a cup label, and each value is the label of the cup that is
@@ -69,7 +22,7 @@
   [links cup]
   (lazy-seq (cons cup (link-seq links (links cup)))))
 
-(defn find-destination-2
+(defn find-destination
   "Given the label of the current cup, the largest cup label, and the
   cups which are currently unavailable because they have been picked
   up, find the label of the destination cup after which they should be
@@ -82,7 +35,7 @@
       (recur (if (> result 1) (dec result) max))
       result)))
 
-(defn move-2
+(defn move
   "Perform a single move in the game of crab cups. Takes the circular
   linked hash map of cups and the label of the current cup. Unlinks
   three cups after the current cup (stitching in the cup which follows
@@ -92,7 +45,7 @@
   [[cups current]]
   (let [removed     (take 3 (drop 1 (link-seq cups current)))
         next        (cups (last removed))
-        destination (find-destination-2 current (count cups) removed)
+        destination (find-destination current (count cups) removed)
         after       (cups destination)]
     [(-> cups
           (assoc current next)
@@ -100,11 +53,22 @@
           (assoc (last removed) after))
      next]))
 
-(defn part-2
-  "Solve part 2: Build the circular linked hash map, play ten million
-  moves, then return the product of two cups that follow cup 1."
+(defn part-1
+  "Solve part 1: Play 100 moves, then return the labels of the cups that
+  follow cup 1 as a string."
   ([]
-   (part-2 part-2-input))
+   (part-1 input))
   ([cups]
-   (let [result (nth (iterate move-2 [(build-links cups) (first cups)]) 10000000)]
+   (let [result (nth (iterate move [(build-links cups) (first cups)]) 100)]
+     (apply str (drop 1 (take (count cups) (link-seq (first result) 1)))))))
+
+(defn part-2
+  "Solve part 2: Expand the list of cups to reach a million, build the
+  circular linked hash map, play ten million moves, then return the
+  product of two cups that follow cup 1."
+  ([]
+   (part-2 input))
+  ([cups]
+   (let [cups (concat cups (range (inc (count cups)) 1000001))
+         result (nth (iterate move [(build-links cups) (first cups)]) 10000000)]
      (apply * (take 2 (drop 1 (link-seq (first result) 1)))))))
--- test/advent_of_code_2020/day_23_test.clj
+++ test/advent_of_code_2020/day_23_test.clj
@@ -1,11 +1,18 @@
 (ns advent-of-code-2020.day-23-test
     "Unit tests for day 23."
   (:require [clojure.test :as test]
-            [advent-of-code-2020.day-23 :as sut]))
+            [advent-of-code-2020.day-23 :as sut]
+            [advent-of-code-2020.day-23-old :as sut-old]))
 
 (def sample-input
   "The cup arrangement from the sample problem."
   [3 8 9 1 2 5 4 6 7])
+
+(test/deftest part-1-sample-old
+  (test/is (= "67384529" (sut-old/part-1 sample-input))))
+
+(test/deftest part-1-old
+  (test/is (= "32658947" (sut-old/part-1))))
 
 (test/deftest part-1-sample
   (test/is (= "67384529" (sut/part-1 sample-input))))
@@ -13,12 +20,8 @@
 (test/deftest part-1
   (test/is (= "32658947" (sut/part-1))))
 
-(def sample-input-2
-  "The huge input for part 2."
-  (concat sample-input (range 10 1000001)))
-
 (test/deftest part-2-sample
-  (test/is (= 149245887792 (sut/part-2 sample-input-2))))
+  (test/is (= 149245887792 (sut/part-2 sample-input))))
 
 (test/deftest part-2
   (test/is (= 683486010900 (sut/part-2))))
