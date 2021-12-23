faa70d1005c5295acb8939d322be8b2d21fd2104
James Elliott <james@deepsymmetry.org>
2021-12-22 19:15:47 -0600

    Add 2021 day 22 part 2.

--- src/advent_of_code_2021/day_22.clj
+++ src/advent_of_code_2021/day_22.clj
@@ -68,3 +68,79 @@
    (part-1 input))
   ([steps]
    (count (boot (sanitize-input steps)))))
+
+(defn intersect?
+  "Checks whether two prisms have any overlapping cubes."
+  [[x-min-1 x-max-1 y-min-1 y-max-1 z-min-1 z-max-1] [x-min-2 x-max-2 y-min-2 y-max-2 z-min-2 z-max-2]]
+  (and (< x-min-2 x-max-1) (> x-max-2 x-min-1)
+       (< y-min-2 y-max-1) (> y-max-2 y-min-1)
+       (< z-min-2 z-max-1) (> z-max-2 z-min-1)))
+
+(defn remove-intersection
+  "If the target prism intersects the cutting prism, split it into the
+  pieces which extend beyond the cutting prism, if any."
+  [cutting-prism target-prism]
+  (if (intersect? target-prism cutting-prism)
+    (let [[x-min-cut x-max-cut y-min-cut y-max-cut z-min-cut z-max-cut] cutting-prism
+          new-prisms                                                    (volatile! [])
+          remaining-prism                                               (volatile! target-prism)]
+      (when (< (get @remaining-prism 0) x-min-cut)  ; Target extends left of cut, split off that chunk.
+        (vswap! new-prisms conj (assoc @remaining-prism 1 x-min-cut))
+        (vswap! remaining-prism assoc 0 x-min-cut))
+      (when (> (get @remaining-prism 1) x-max-cut)  ; Target extends right of cut, split off that chunk.
+        (vswap! new-prisms conj (assoc @remaining-prism 0 x-max-cut))
+        (vswap! remaining-prism assoc 1 x-max-cut))
+      (when (< (get @remaining-prism 2) y-min-cut)  ; Target extends below cut, split off that chunk.
+        (vswap! new-prisms conj (assoc @remaining-prism 3 y-min-cut))
+        (vswap! remaining-prism assoc 2 y-min-cut))
+      (when (> (get @remaining-prism 3) y-max-cut)  ; Target extends above cut, split off that chunk.
+        (vswap! new-prisms conj (assoc @remaining-prism 2 y-max-cut))
+        (vswap! remaining-prism assoc 3 y-max-cut))
+      (when (< (get @remaining-prism 4) z-min-cut)  ; Target extends behind cut, split off that chunk.
+        (vswap! new-prisms conj (assoc @remaining-prism 5 z-min-cut))
+        (vswap! remaining-prism assoc 4 z-min-cut))
+      (when (> (get @remaining-prism 5) z-max-cut)  ; Target extends in front of cut, split off that chunk.
+        (vswap! new-prisms conj (assoc @remaining-prism 4 z-max-cut))
+        (vswap! remaining-prism assoc 5 z-max-cut))
+      @new-prisms)
+    [target-prism]))
+
+(defn boot-step-2
+  "Run one step of the boot sequence, using constructive solid
+  geometry."
+  [prisms [action x-min x-max y-min y-max z-min z-max]]
+  ;; The bounds of our prism are from the minimum coordinate of each
+  ;; contained cube, to one greater than the maximum coordinate of
+  ;; each contained cube, to make the volume computation and
+  ;; intersection/splitting logic work. I forgot to add that increment
+  ;; initially, and could not figure out for a good while why my
+  ;; answers were coming in too low.
+  (let [cutting-prism [x-min (inc x-max) y-min (inc y-max) z-min (inc z-max)]
+        cut-prisms    (mapcat (partial remove-intersection cutting-prism) prisms)]
+    (case action
+      "on"  (conj cut-prisms cutting-prism)
+      "off" cut-prisms)))
+
+(defn boot-2
+  "Run the boot sequence using constructive geometry, returning the
+  resulting list of prisms."
+  [steps]
+  (reduce boot-step-2
+          []
+          steps))
+
+(defn count-cubes
+  "Counts the number of unit cubes contained in the specified list of
+  rectangular prisms."
+  [prisms]
+  (reduce (fn [acc [x-min x-max y-min y-max z-min z-max]]
+            (+ acc (* (- x-max x-min) (- y-max y-min) (- z-max z-min))))
+          0
+          prisms))
+
+(defn part-2
+  "Solve part 2."
+  ([]
+   (part-2 input))
+  ([steps]
+   (count-cubes (boot-2 steps))))
--- test/advent_of_code_2021/day_22_test.clj
+++ test/advent_of_code_2021/day_22_test.clj
@@ -9,3 +9,11 @@
 (test/deftest part-1
   (test/is (= 590784 (sut/part-1 sample-input)))
   (test/is (= 545118 (sut/part-1))))
+
+(def sample-input-2
+  (sut/read-input "2021/day_22_sample_2.txt"))
+
+(test/deftest part-2
+  (test/is (= 474140 (sut/part-1 sample-input-2)))
+  (test/is (= 2758514936282235 (sut/part-2 sample-input-2)))
+  (test/is (= 1227298136842375 (sut/part-2))))
